59ac1308f195f72f6313600a2ecd74cfd83c1ad6
Quang Son Le <quangle.bxl@gmail.com>
2018-11-27 11:28:12 +0100

    fix pkg vulnerability

--- Node_Server/server.js
+++ Node_Server/server.js
@@ -4,7 +4,11 @@ const app=express();
 const dotenv=require('dotenv').config();
 
 //please secure this
-let url = "mongodb://localhost:27017/dorothycares";
+//uncomment when DB address onserver is solved
+//let url = "mongodb://localhost:27017/dorothycares";
+//MLAB connection not suitable for prod
+let url='mongodb://sonnyboy:lovelace2@ds063889.mlab.com:63889/dorothytest';
+
 const port = process.env.PORT || 5000;
 
 var myDB;//global var for the DB, not so clean
